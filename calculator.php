4160cd2c1b14c4ec380fa932d6d80b6d05dbda6c
msm304 <soltani5747@gmail.com>
2023-03-05 17:32:55 +0330

    write php code calculator

--- calculator.php
+++ calculator.php
@@ -1,1 +1,24 @@
 <?php
+if(isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operator'])) {
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operator = $_POST['operator'];
+   switch($operator) {
+    case '+':
+        $result = $num1 + $num2;
+        break;
+    case '-':
+        $result = $num1 - $num2;
+        break;
+    case '*':
+        $result = $num1 * $num2;
+        break;
+    case '/';
+        $result = $num1 / $num2;
+        break;
+    default:
+        $result = 0;
+        break;
+   } 
+}
+?>
