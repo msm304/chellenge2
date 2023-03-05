4bbdd1a195bebb5e7d39e3c13f854af6457563cc
msm304 <soltani5747@gmail.com>
2023-03-05 18:36:56 +0330

    complete calculator chellenge

--- calculator.php
+++ calculator.php
@@ -1,24 +1,36 @@
 <?php
-if(isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operator'])) {
+if (isset($_POST['num1']) && isset($_POST['num2']) && isset($_POST['operator'])) {
     $num1 = $_POST['num1'];
     $num2 = $_POST['num2'];
     $operator = $_POST['operator'];
-   switch($operator) {
-    case '+':
-        $result = $num1 + $num2;
-        break;
-    case '-':
-        $result = $num1 - $num2;
-        break;
-    case '*':
-        $result = $num1 * $num2;
-        break;
-    case '/';
-        $result = $num1 / $num2;
-        break;
-    default:
-        $result = 0;
-        break;
-   } 
+    switch ($operator) {
+        case '+':
+            $result = $num1 + $num2;
+            break;
+        case '-':
+            $result = $num1 - $num2;
+            break;
+        case '*':
+            $result = $num1 * $num2;
+            break;
+        case '/';
+            $result = $num1 / $num2;
+            break;
+        default:
+            $result = 0;
+            break;
+    }
 }
 ?>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    <input type="number" name="num1" value="<?php echo $num1 ?? ''; ?>">
+    <select name="operator">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+    </select>
+    <input type="number" name="num2" value="<?php echo $num2 ?? ''; ?>">
+    <input type="submit" value="Calculate">
+</form>
+<?php if (isset($result)) echo "Result: $result"; ?>
